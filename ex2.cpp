230aad99806c750cf91de84446b980db819554a3
vinip000 <112911506+vinip000@users.noreply.github.com>
2023-10-16 12:40:33 -0300

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool verificar(string palavra) {
+  if (palavra.size() <= 1) {
+    return true; // Caso base: uma letra é um palíndromo.
+  }
+
+  if (palavra[0] == palavra.back() && verificar(palavra.substr(1, palavra.length() - 2))) {
+    return true;
+  }
+
+  return false; // Se a condição acima não for atendida, não é um palíndromo.
+}
+
+int main() {
+  string palavra;
+  cout << "Digite uma palavra: " << endl;
+  cin >> palavra;
+
+  if (verificar(palavra)) {
+    cout << "A palavra é um palíndromo." << endl;
+  }
+  else {
+    cout << "A palavra não é um palíndromo." << endl;
+  }
+
+  return 0;
+}
